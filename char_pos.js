0d997bdb325dc81e975a561e4f133c138f93ec5f
tian <tianweirandai@gmail.com>
2017-05-31 21:55:40 +0000

    remote indcies

---
+++ char_pos.js
@@ -0,0 +1,24 @@
+
+
+function charPos(str){
+  var strT = str.replace(/\s/g, ""); // get rid of all the spaces
+  strT = strT.split(""); // turn a string into a char array
+  var result = {};
+
+  for(var i = 0; i < strT.length; i++){
+      var temp = strT[i];
+      if(result[temp]) { // if the field already exists if there is duplicate of this char.
+          result[temp].push(i);
+      }
+      else{ // the field does not exitst, then create a field and set the count to 1
+        result[temp] = new Array();
+        result[temp].push(i);
+      }
+  }
+  return result;
+}
+
+
+var letters = "lighthouse in the house";
+//var letters = "abcd";
+console.log(charPos(letters));
